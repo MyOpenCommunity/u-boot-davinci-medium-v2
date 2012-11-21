720fd644f6380133a40d3a5d3d51fb9af678bcd9
Davide Bonfanti <davide.bonfanti@bticino.it>
2012-11-21 11:25:36 +0100

    console: disabling silent if bootcmd not present
    Signed-off-by: Davide Bonfanti <davide.bonfanti@bticino.it>
    Signed-off-by: Raffaele Recalcati <raffaele.recalcati@bticino.it>

--- common/main.c
+++ common/main.c
@@ -484,6 +484,9 @@ void main_loop (void)
 
 	debug ("### main_loop: bootcmd=\"%s\"\n", s ? s : "<UNDEFINED>");
 
+	if (!s)
+		gd->flags &= ~GD_FLG_SILENT; /* Disabling silent if not bootcmd */
+
 	if (bootdelay != -1 && s && !abortboot(bootdelay)) {
 # ifdef CONFIG_AUTOBOOT_KEYED
 		int prev = disable_ctrlc(1);	/* disable Control C checking */
